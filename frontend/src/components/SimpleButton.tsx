faf9e42ab3e9fd3c393fbfc7e29cf599bbcce2c9
elomarii <i.ibrahim.elomari@gmail.com>
2025-09-24 19:41:30 +0200

    theme update

--- frontend/src/components/SimpleButton.tsx
+++ frontend/src/components/SimpleButton.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@headlessui/react"
 import { MouseEventHandler } from "react"
 
-const buttonStyle = "inline-flex justify-start gap-x-1.5 rounded-md bg-white/10 px-3 py-1 my-1 text-sm inset-ring-1 inset-ring-white/5 hover:bg-white/20"
+const buttonStyle = "inline-flex justify-start gap-x-1.5 rounded-md bg-white/10 px-3 py-1 my-1 text-sm w-fit inset-ring-1 inset-ring-white/5 hover:bg-white/20"
 
 interface ButtonProps {
     text: string
